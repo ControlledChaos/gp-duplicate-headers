e56efe2e125ac35f91af3a73d8aadb384aa2aff3
Greg Sweet <greg@ccdzine.com>
2017-10-19 17:20:50 -0700

    Tidy up the code spacing.

--- gp-duplicate-headers.php
+++ gp-duplicate-headers.php
@@ -36,20 +36,20 @@ class GP_Duplicate_Headers_Functions {
 	 */
 	function generate_page_header_duplicate() {
 		global $wpdb;
-		if (! ( isset( $_GET['post']) || isset( $_POST['post'])  || ( isset($_REQUEST['action']) && 'generate_page_header_duplicate' == $_REQUEST['action'] ) ) ) {
-			wp_die('No page header to duplicate has been supplied!');
+		if ( ! ( isset( $_GET['post'] ) || isset( $_POST['post'] )  || ( isset($_REQUEST['action'] ) && 'generate_page_header_duplicate' == $_REQUEST['action'] ) ) ) {
+			wp_die( 'No page header to duplicate has been supplied!' );
 		}
 
 		/**
 		 * Nonce verification.
 		 */
-		if ( !isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
+		if ( ! isset( $_GET['duplicate_nonce'] ) || !wp_verify_nonce( $_GET['duplicate_nonce'], basename( __FILE__ ) ) )
 			return;
 
 		/**
 		 * Get the original post id.
 		 */
-		$post_id = (isset($_GET['post']) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
+		$post_id = ( isset( $_GET['post'] ) ? absint( $_GET['post'] ) : absint( $_POST['post'] ) );
 
 		/**
 		 * And all the original post data then.
@@ -60,13 +60,13 @@ class GP_Duplicate_Headers_Functions {
 		 * If you don't want current user to be the new post author,
 		 * then change next couple of lines to this: $new_post_author = $post->post_author;
 		 */
-		$current_user = wp_get_current_user();
+		$current_user    = wp_get_current_user();
 		$new_post_author = $current_user->ID;
 
 		/**
 		 * If post data exists, create the post duplicate.
 		 */
-		if (isset( $post ) && $post != null) {
+		if ( isset( $post ) && $post != null ) {
 
 			/**
 			 * New post data array.
@@ -95,26 +95,26 @@ class GP_Duplicate_Headers_Functions {
 			/**
 			 * Get all current post terms ad set them to the new post draft.
 			 */
-			$taxonomies = get_object_taxonomies($post->post_type); // Returns array of taxonomy names for post type, ex array("category", "post_tag");
-			foreach ($taxonomies as $taxonomy) {
-				$post_terms = wp_get_object_terms($post_id, $taxonomy, array('fields' => 'slugs'));
-				wp_set_object_terms($new_post_id, $post_terms, $taxonomy, false);
+			$taxonomies = get_object_taxonomies( $post->post_type ); // Returns array of taxonomy names for post type, ex array("category", "post_tag");
+			foreach ( $taxonomies as $taxonomy ) {
+				$post_terms = wp_get_object_terms( $post_id, $taxonomy, array( 'fields' => 'slugs' ) );
+				wp_set_object_terms( $new_post_id, $post_terms, $taxonomy, false );
 			}
 
 			/**
 			 * Duplicate all post meta just in two SQL queries.
 			 */
-			$post_meta_infos = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id");
-			if (count($post_meta_infos)!=0) {
-				$sql_query = "INSERT INTO $wpdb->postmeta (post_id, meta_key, meta_value) ";
-				foreach ($post_meta_infos as $meta_info) {
+			$post_meta_infos = $wpdb->get_results( "SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id=$post_id" );
+			if ( count $post_meta_infos ) !=0 ) {
+				$sql_query = "INSERT INTO $wpdb->postmeta ( post_id, meta_key, meta_value ) ";
+				foreach ( $post_meta_infos as $meta_info ) {
 					$meta_key = $meta_info->meta_key;
-					if( $meta_key == '_wp_old_slug' ) continue;
+					if ( $meta_key == '_wp_old_slug' ) continue;
 					$meta_value = addslashes($meta_info->meta_value);
 					$sql_query_sel[]= "SELECT $new_post_id, '$meta_key', '$meta_value'";
 				}
-				$sql_query.= implode(" UNION ALL ", $sql_query_sel);
-				$wpdb->query($sql_query);
+				$sql_query.= implode( " UNION ALL ", $sql_query_sel );
+				$wpdb->query( $sql_query );
 			}
 
 
@@ -124,7 +124,7 @@ class GP_Duplicate_Headers_Functions {
 			wp_redirect( admin_url( 'post.php?action=edit&post=' . $new_post_id ) );
 			exit;
 		} else {
-			wp_die('Header creation failed, could not find original post: ' . $post_id);
+			wp_die( 'Header creation failed, could not find original post: ' . $post_id );
 		}
 	}
 
@@ -132,7 +132,7 @@ class GP_Duplicate_Headers_Functions {
 	 * Add the duplicate link to action list for post_row_actions.
 	 */
 	function generate_page_header_duplicate_post_link( $actions, $post ) {
-		if ($post->post_type=='generate_page_header' && current_user_can('edit_posts')) {
+		if ( $post->post_type=='generate_page_header' && current_user_can( 'edit_posts' ) ) {
 			$actions['duplicate'] = '<a href="' . wp_nonce_url('admin.php?action=generate_page_header_duplicate&post=' . $post->ID, basename(__FILE__), 'duplicate_nonce' ) . '" title="Duplicate this Page Header" rel="permalink">Duplicate</a>';
 		}
 		return $actions;
